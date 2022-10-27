08dbb5503b83ad2c07baa826ea916ef75b47b579
lawrencedulian <lawrenceedulian@gmail.com>
2022-10-27 17:17:45 +0200

    added to data empty array

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const { createApp } = Vue;
+
+createApp ({
+    data () {
+        return {
+            newTask: "",
+            tasks: []
+        }
+    },
+
+    methods: {
+       
+    }
+}).mount("#app")
