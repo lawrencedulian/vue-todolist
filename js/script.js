5f10387bbb965bc586492ade71ed68205507b710
lawrencedulian <lawrenceedulian@gmail.com>
2022-10-27 17:31:59 +0200

    added to methods - addNewTask

--- js/script.js
+++ js/script.js
@@ -4,11 +4,20 @@ createApp ({
     data () {
         return {
             newTask: "",
-            tasks: []
+            tasks: [],
+            hasError: false
         }
     },
 
     methods: {
-       
+        addNewTask() {
+            if (this.newTask.lenght <= 5) {
+                this.hasError = true;
+            } else {
+                this.tasks.unshift(this.newTask);
+                this.newTask = "";
+                this.hasError = false;
+            }
+        }
     }
 }).mount("#app")
